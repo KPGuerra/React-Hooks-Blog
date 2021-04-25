7e36c9d2773be9ed584df2f44b9fdbc84f16bcdb
KPGuerra <KPGuerra96@gmail.com>
2021-04-24 20:43:15 -0400

    useState Button Functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React , { useState } from 'react'
 
 function App() {
+  //Declaring a state variable
+  const [count, setCount] = useState(5)
+
+  function decreaseCount() {
+    setCount(prevCount => prevCount - 1 )
+  }
+
+  function increaseCount() {
+    setCount(prevCount => prevCount + 1 )
+  }
   return (
     <div>
       <h1> Basic Counter</h1>
       
-      <button> - </button>
-      <span> 0 </span>
-      <button> + </button>
+      <button onClick={decreaseCount}> - </button>
+      <span> {count} </span>
+      <button onClick={increaseCount}> + </button>
     </div>
   );
 }
 
 export default App;
+
+
